e2c3c10bec14dc88b5b3e98d6126d0b25f1dcf2d
ArckyPN <archsyker@gmail.com>
2025-09-17 15:44:25 +0200

    adjusted to new VarInt trait

--- varint_core/src/lib.rs
+++ varint_core/src/lib.rs
@@ -1,6 +1,29 @@
-mod types;
+mod io;
+pub mod types;
 
+pub use io::{
+    reader::{Reader, ReferenceReader},
+    writer::{ReferenceWriter, Writer},
+};
 pub use types::{BitNumber, BitRange, Number};
 
 #[cfg(feature = "moq")]
 pub use types::{BinaryData, Tuple};
+
+pub trait VarInt {
+    /// The data type to decode.
+    type Item;
+
+    /// The error type.
+    type Error;
+
+    /// Encode the data for transmission.
+    fn encode<W>(&self, writer: &mut W) -> Result<(), Self::Error>
+    where
+        W: Writer;
+
+    /// Decode received data.
+    fn decode<R>(reader: &mut R) -> Result<Self::Item, Self::Error>
+    where
+        R: Reader;
+}
--- varint_core/src/types/number.rs
+++ varint_core/src/types/number.rs
@@ -3,7 +3,10 @@ use std::fmt::{Debug, Display};
 use bitvec::prelude::*;
 use snafu::{ResultExt, Snafu};
 
-use crate::VarInt;
+use crate::{
+    VarInt,
+    io::{reader::ReaderError, writer::WriterError},
+};
 
 const MAX_U6: u64 = (2 << 5) - 1;
 const MAX_U14: u64 = (2 << 13) - 1;
@@ -45,6 +48,7 @@ impl Number {
         this.set_value(v.into()).expect("value will fit");
         this
     }
+
     /// Returns the value of the VarInt.
     ///
     /// ```
@@ -76,7 +80,6 @@ impl Number {
     ///
     /// ```
     /// # use varint_core::Number;
-    /// # use varint_core::error::NumberTooLarge;
     /// let mut v = Number::default();
     /// let err = v.set_value(u64::MAX);
     /// assert!(err.is_err());
@@ -116,49 +119,68 @@ impl Debug for Number {
     }
 }
 
+#[derive(Debug, Snafu, PartialEq, Clone)]
+/// Error when [VarInt](crate::VarInt) implementation fails.
+pub enum NumberError {
+    ReaderError {
+        source: ReaderError,
+    },
+    WriterError {
+        source: WriterError,
+    },
+
+    /// empty buffer given to [decode](crate::VarInt::decode)
+    #[snafu(display("unable to decode data from empty buffer"))]
+    NoData,
+    /// tried to create a VarInt with a too large number
+    #[snafu(display("number >{num}< is too large"))]
+    TooLarge {
+        num: u64,
+        source: NumberTooLarge,
+    },
+}
+
 impl VarInt for Number {
     type Error = NumberError;
     type Item = Self;
     fn decode<R>(reader: &mut R) -> Result<Self::Item, Self::Error>
     where
-        R: bytes::Buf,
+        R: crate::Reader,
     {
-        snafu::ensure!(reader.has_remaining(), NoDataSnafu);
-
         // first byte contains the size and (part
         // of) the number
-        let first = reader.get_u8();
+        let byte = reader.read_bytes(1).context(ReaderSnafu)?[0];
 
         // first two bits denote how many bits are
         // part of the number
-        let size = (first & 0b1100_0000) >> 6;
+        let size = (byte & 0b1100_0000) >> 6;
 
         // start of the number
-        let mut buf = [0u8; 8];
-        buf[0] = first & 0b0011_1111;
+        let byte = byte & 0b0011_1111;
 
         let num = match size {
-            0b00 => buf[0] as u64,
+            0b00 => byte as u64,
             0b01 => {
                 // one more byte needed
-                snafu::ensure!(reader.has_remaining(), NoDataSnafu);
+                let tail = reader.read_bytes(1).context(ReaderSnafu)?[0];
 
-                reader.copy_to_slice(buf[1..2].as_mut());
-                u16::from_be_bytes(buf[..2].try_into().unwrap()) as u64
+                u16::from_be_bytes([byte, tail]) as u64
             }
             0b10 => {
                 // three more byte needed
-                snafu::ensure!(reader.remaining() >= 3, NoDataSnafu);
+                let tail = reader.read_bytes(3).context(ReaderSnafu)?;
+                let mut buf = vec![byte];
+                buf.append(&mut tail.into());
 
-                reader.copy_to_slice(buf[1..4].as_mut());
-                u32::from_be_bytes(buf[..4].try_into().unwrap()) as u64
+                u32::from_be_bytes(buf.try_into().expect("buf has len 4")) as u64
             }
             0b11 => {
                 // seven more byte needed
-                snafu::ensure!(reader.remaining() >= 7, NoDataSnafu);
+                let tail = reader.read_bytes(7).context(ReaderSnafu)?;
+                let mut buf = vec![byte];
+                buf.append(&mut tail.into());
 
-                reader.copy_to_slice(buf[1..8].as_mut());
-                u64::from_be_bytes(buf)
+                u64::from_be_bytes(buf.try_into().expect("buf has len 8"))
             }
             _ => unreachable!("impossible size"),
         };
@@ -169,19 +191,97 @@ impl VarInt for Number {
         Ok(v)
     }
 
-    fn encode(&self) -> Result<bytes::Bytes, Self::Error> {
-        // decoding in reverse
+    fn encode<W>(&self, writer: &mut W) -> Result<(), Self::Error>
+    where
+        W: crate::Writer,
+    {
         let value = self.value();
-        Ok(if value <= MAX_U6 {
-            (value as u8).to_be_bytes().to_vec().into()
+        let buf = if value <= MAX_U6 {
+            (value as u8).to_be_bytes().to_vec()
         } else if value <= MAX_U14 {
-            (0b01 << 14 | (value as u16)).to_be_bytes().to_vec().into()
+            (0b01 << 14 | (value as u16)).to_be_bytes().to_vec()
         } else if value <= MAX_U30 {
-            (0b10 << 30 | (value as u32)).to_be_bytes().to_vec().into()
+            (0b10 << 30 | (value as u32)).to_be_bytes().to_vec()
         } else {
-            (0b11 << 62 | value).to_be_bytes().to_vec().into()
-        })
+            (0b11 << 62 | value).to_be_bytes().to_vec()
+        };
+        writer.write_bytes(&buf).context(WriterSnafu)?;
+        Ok(())
     }
+}
+
+macro_rules! impl_partial_eq {
+    ( $($prim:ty),+ $(,)? ) => {
+        $(
+            impl PartialEq<$prim> for Number {
+                fn eq(&self, other: &$prim) -> bool {
+                    *other as u64 == self.value()
+                }
+            }
+            impl PartialEq<Number> for $prim {
+                fn eq(&self, other: &Number) -> bool {
+                    *self as u64 == other.value()
+                }
+            }
+        )+
+    };
+}
+impl_partial_eq!(
+    u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize, bool
+);
+
+// TODO checks these again
+macro_rules! impl_from {
+    ( $typ:ty ; $($prim:ty),+ $(,)? ) => {
+        $(
+            impl From<$prim> for $typ {
+                fn from(value: $prim) -> Self {
+                    let mut v = Self::default();
+                    v.set_value(value.into())
+                        .expect("value cannot be larger than the maximum");
+                    v
+                }
+            }
+        )+
+    };
+}
+impl_from!(Number; u8, u16, u32);
+
+macro_rules! impl_try_from {
+    ( $typ:ty ; $($prim:ty),+ $(,)? ) => {
+        $(
+            impl TryFrom<$prim> for $typ {
+                type Error = ConversionError;
+                fn try_from(value: $prim) -> Result<Self, Self::Error> {
+                    let value = if value.lt(&value) {
+                        return Err(ConversionError::TooLargeUnsigned { num: value as u128 });
+                    } else {
+                        value as i128
+                    };
+                    snafu::ensure!(value >= 0, NegativeSnafu { num: value });
+
+                    let mut v = Number::default();
+                    v.set_value(value as u64)
+                        .context(TooLargeTODOSnafu { num: value })?;
+                    Ok(v)
+                }
+            }
+        )+
+    };
+}
+impl_try_from!(Number; u64, u128, usize, i8, i16, i32, i64, i128, isize);
+
+impl From<Number> for u64 {
+    fn from(value: Number) -> Self {
+        value.value()
+    }
+}
+
+#[derive(Debug, Snafu, PartialEq, Clone)]
+pub enum ConversionError {
+    Negative { num: i128 },
+    TooLargeTODO { num: i128, source: NumberTooLarge },
+    TooLargeUnsigned { num: u128 },
 }
 
 /// Number is larger than the maximum possible value of an VarInt
@@ -191,20 +291,9 @@ pub struct NumberTooLarge {
     pub(crate) num: u64,
 }
 
-#[derive(Debug, Snafu, PartialEq, Clone)]
-/// Error when [VarInt](crate::VarInt) implementation fails.
-pub enum NumberError {
-    /// empty buffer given to [decode](crate::VarInt::decode)
-    #[snafu(display("unable to decode data from empty buffer"))]
-    NoData,
-    /// tried to create a VarInt with a too large number
-    #[snafu(display("number >{num}< is too large"))]
-    TooLarge { num: u64, source: NumberTooLarge },
-}
-
 #[cfg(test)]
 mod tests {
-    use bytes::Bytes;
+    use crate::{ReferenceReader, ReferenceWriter, Writer};
 
     use super::*;
 
@@ -226,41 +315,54 @@ mod tests {
     const VALID_NUM_U30: u32 = 524_288;
     const VALID_NUM_U62: u64 = 2_251_799_813_685_248;
 
+    // TODO once Reader and Writer are working
     #[test]
     fn decode_test() {
-        let mut reader = Bytes::from(VALID_U6_BUF.to_vec());
+        let buf = [
+            VALID_U6_BUF.to_vec(),
+            VALID_U14_BUF.to_vec(),
+            VALID_U30_BUF.to_vec(),
+            VALID_U62_BUF.to_vec(),
+        ]
+        .concat();
+        let mut reader = ReferenceReader::new(&buf);
+
         let valid = Number::decode(&mut reader).unwrap();
         assert_eq!(valid, VALID_NUM_U6);
 
-        let mut reader = Bytes::from(VALID_U14_BUF.to_vec());
         let valid = Number::decode(&mut reader).unwrap();
         assert_eq!(valid, VALID_NUM_U14);
 
-        let mut reader = Bytes::from(VALID_U30_BUF.to_vec());
         let valid = Number::decode(&mut reader).unwrap();
         assert_eq!(valid, VALID_NUM_U30);
 
-        let mut reader = Bytes::from(VALID_U62_BUF.to_vec());
         let valid = Number::decode(&mut reader).unwrap();
         assert_eq!(valid, VALID_NUM_U62);
     }
 
     #[test]
     fn encode_test() {
+        let buf = [
+            VALID_U6_BUF.to_vec(),
+            VALID_U14_BUF.to_vec(),
+            VALID_U30_BUF.to_vec(),
+            VALID_U62_BUF.to_vec(),
+        ]
+        .concat();
+        let mut writer = ReferenceWriter::new();
+
         let num = Number::from(VALID_NUM_U6);
-        let valid = num.encode().unwrap();
-        assert_eq!(valid, VALID_U6_BUF.to_vec());
+        num.encode(&mut writer).unwrap();
 
         let num = Number::from(VALID_NUM_U14);
-        let valid = num.encode().unwrap();
-        assert_eq!(valid, VALID_U14_BUF.to_vec());
+        num.encode(&mut writer).unwrap();
 
         let num = Number::from(VALID_NUM_U30);
-        let valid = num.encode().unwrap();
-        assert_eq!(valid, VALID_U30_BUF.to_vec());
+        num.encode(&mut writer).unwrap();
 
         let num = Number::try_from(VALID_NUM_U62).unwrap();
-        let valid = num.encode().unwrap();
-        assert_eq!(valid, VALID_U62_BUF.to_vec());
+        num.encode(&mut writer).unwrap();
+
+        assert_eq!(writer.finish().unwrap(), buf);
     }
 }
