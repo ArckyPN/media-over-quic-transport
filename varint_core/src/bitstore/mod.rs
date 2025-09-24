780471e04069698b339bc7b621f36667286957f3
ArckyPN <archsyker@gmail.com>
2025-09-24 14:57:54 +0200

    added custom bitvec replacement

---
+++ varint_core/src/bitstore/mod.rs
@@ -0,0 +1,253 @@
+mod utils;
+
+use std::fmt::Debug;
+
+use funty::Unsigned;
+
+use bytes::Bytes;
+use snafu::{ResultExt, Snafu};
+
+#[derive(Clone, PartialEq, PartialOrd)]
+pub struct BitStore<const MIN: usize = 0, const MAX: usize = { usize::MAX }> {
+    data: Bytes,
+    len: usize,
+}
+
+impl<const MIN: usize, const MAX: usize> BitStore<MIN, MAX> {
+    pub fn new_bits(buf: &[u8], bits: usize) -> Result<Self, Error> {
+        let mut this = Self::default();
+        this.set_bits(buf, bits)?;
+        Ok(this)
+    }
+
+    pub fn new_number<U>(num: U, bits: Option<usize>) -> Result<Self, Error>
+    where
+        U: Unsigned,
+    {
+        let mut this = Self::default();
+        this.set_number(num, bits)?;
+        Ok(this)
+    }
+
+    pub fn set_bits(&mut self, buf: &[u8], bits: usize) -> Result<&mut Self, Error> {
+        Self::ensure_fit(bits).context(InvalidLengthSnafu)?;
+
+        self.data = Bytes::copy_from_slice(buf);
+        self.len = bits;
+
+        Ok(self)
+    }
+
+    pub fn set_number<U>(&mut self, num: U, bits: Option<usize>) -> Result<&mut Self, Error>
+    where
+        U: Unsigned,
+    {
+        let num_bits = utils::num_bits(num);
+        let bits = match bits {
+            Some(b) => {
+                // check if number can fit into the expected space
+                if b < num_bits {
+                    return Err(Error::InvalidFit {
+                        num: num.as_u128(),
+                        needs: num_bits,
+                        expected: b,
+                    });
+                }
+                b
+            }
+            None => num_bits,
+        };
+        Self::ensure_fit(bits).context(InvalidLengthSnafu)?;
+
+        self.len = bits;
+
+        // remove superfluous preceding bytes
+        let bytes = if self.len % 8 != 0 {
+            self.len / 8 + 1
+        } else {
+            self.len / 8
+        };
+
+        let mut bytes = match self.len {
+            0 => todo!("Error or what to do in this case?"),
+            1..9 => num.as_u8().to_be_bytes().to_vec(),
+            9..17 => num.as_u16().to_be_bytes()[2 - bytes..].to_vec(),
+            17..33 => num.as_u32().to_be_bytes()[4 - bytes..].to_vec(),
+            33..65 => num.as_u64().to_be_bytes()[8 - bytes..].to_vec(),
+            65..129 => num.as_u128().to_be_bytes()[16 - bytes..].to_vec(),
+            _ => return Err(Error::TooLarge),
+        };
+
+        if self.len % 8 != 0 {
+            utils::shift_bits(&mut bytes, self.len);
+        }
+
+        self.data = Bytes::copy_from_slice(&bytes);
+
+        Ok(self)
+    }
+
+    pub fn bits(&self) -> Bytes {
+        self.data.clone()
+    }
+
+    pub fn number<U>(&self) -> U
+    where
+        U: Unsigned,
+    {
+        let mut buf = self.data.to_vec();
+        utils::unshift_bits(&mut buf, self.len);
+
+        let len = buf.len();
+        match len {
+            0 => U::try_from(0u8).unwrap_or_default(),
+            1 => U::try_from(u8::from_be_bytes(buf.try_into().expect("len 1 verified")))
+                .unwrap_or_default(),
+            2 => U::try_from(u16::from_be_bytes(buf.try_into().expect("len 2 verified")))
+                .unwrap_or_default(),
+            3..5 => {
+                let mut vec = vec![0; 4 - len];
+                vec.extend_from_slice(&buf);
+                U::try_from(u32::from_be_bytes(vec.try_into().expect("len 4 verified")))
+                    .unwrap_or_default()
+            }
+            5..9 => {
+                let mut vec = vec![0; 8 - len];
+                vec.extend_from_slice(&buf);
+                U::try_from(u64::from_be_bytes(vec.try_into().expect("len 8 verified")))
+                    .unwrap_or_default()
+            }
+            9..17 => {
+                let mut vec = vec![0; 16 - len];
+                vec.extend_from_slice(&buf);
+                U::try_from(u128::from_be_bytes(
+                    vec.try_into().expect("len 16 verified"),
+                ))
+                .unwrap_or_default()
+            }
+            _ => unreachable!("number above u128 are not supported"),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    fn ensure_fit(bits: usize) -> Result<(), InvalidBitLength> {
+        if bits < MIN || bits > MAX {
+            Err(InvalidBitLength {
+                got: bits,
+                min: MIN,
+                max: MAX,
+            })
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl<const MIN: usize, const MAX: usize> Default for BitStore<MIN, MAX> {
+    fn default() -> Self {
+        let bytes = MIN / 8;
+        let bits = MIN % 8;
+
+        let len = if bits != 0 { bytes + 1 } else { bytes };
+
+        // init with minimum number of bytes allocated
+        Self {
+            data: Bytes::copy_from_slice(&vec![0; len]),
+            len: 0,
+        }
+    }
+}
+
+impl<const MIN: usize, const MAX: usize> Debug for BitStore<MIN, MAX> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BitStore")
+            .field("len", &self.len)
+            .field(
+                "binary",
+                &self
+                    .data
+                    .iter()
+                    .map(|byte| format!("{byte:08b}"))
+                    .collect::<Vec<String>>()
+                    .join(" "),
+            )
+            .finish()
+    }
+}
+
+#[derive(Debug, Snafu, Clone, PartialEq)]
+pub enum Error {
+    /// length is not between the required bounds
+    #[snafu(display("invalid length"))]
+    InvalidLength { source: InvalidBitLength },
+    /// trying to fit a number into too small a space
+    #[snafu(display(
+        "number >{num}< needs >{needs}< bits, but tried to fit into >{expected}< bits"
+    ))]
+    InvalidFit {
+        num: u128,
+        needs: usize,
+        expected: usize,
+    },
+    /// trying to set a number beyond 128 bits
+    #[snafu(display("numbers larger than u128 are not supported"))]
+    TooLarge,
+}
+
+/// length is not between the required bounds
+#[derive(Debug, Snafu, Clone, Copy, PartialEq)]
+#[snafu(display("invalid length, got >{got}<, but need between >{min}< and >{max}<"))]
+pub struct InvalidBitLength {
+    got: usize,
+    min: usize,
+    max: usize,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_test() {
+        let store = BitStore::<8, 16>::default();
+        assert_eq!(store.data.len(), 1);
+        assert_eq!(*store.data, [0]);
+
+        let store = BitStore::<12, 64>::default();
+        assert_eq!(store.data.len(), 2);
+        assert_eq!(*store.data, [0, 0]);
+    }
+
+    #[test]
+    fn set_bits_test() {
+        let mut store = BitStore::<0, 16>::default();
+
+        store.set_bits(&[0b1100_0000], 5).unwrap();
+        assert_eq!(store.number::<u8>(), 0b0001_1000);
+        assert_eq!(*store.bits(), [0b1100_0000]);
+    }
+
+    #[test]
+    fn set_number_test() {
+        let mut store = BitStore::<0, 160>::default();
+
+        store.set_number(4u8, Some(5)).unwrap();
+        assert_eq!(store.number::<u8>(), 4);
+        assert_eq!(*store.bits(), [0b0010_0000]);
+
+        store.set_number(4u8, Some(8)).unwrap();
+        assert_eq!(store.number::<u8>(), 4);
+        assert_eq!(*store.bits(), [4]);
+
+        store.set_number(42u16, Some(11)).unwrap();
+        assert_eq!(store.number::<u32>(), 42);
+        assert_eq!(*store.bits(), [0b0000_0101, 0b0100_0000]);
+
+        store.set_number(512u16, Some(15)).unwrap();
+        assert_eq!(store.number::<u32>(), 512);
+        assert_eq!(*store.bits(), [0b0000_0100, 0b0000_0000]);
+    }
+}
---
+++ varint_core/src/bitstore/utils.rs
@@ -0,0 +1,119 @@
+use funty::Unsigned;
+
+/// Returns the number of bits required to
+/// stored `v`.
+pub fn num_bits<U>(v: U) -> usize
+where
+    U: Unsigned,
+{
+    let ones = v.count_ones();
+    if ones == 0 {
+        return 1;
+    }
+    (v.count_ones() + v.count_zeros() - v.leading_zeros()) as usize
+}
+
+/// Shifts `len` bits of `buf` to the left to align
+/// them to the left.
+pub fn shift_bits(buf: &mut [u8], len: usize) {
+    let n = 8 - (len % 8);
+
+    // no shift required when already on a byte boundary
+    if n == 8 {
+        return;
+    }
+
+    let len = buf.len();
+
+    // shift full buffer by n to the left
+    for i in 0..len {
+        // shift lower half to he left
+        buf[i] <<= n;
+
+        // skip last step for final byte
+        if i == len - 1 {
+            continue;
+        }
+
+        // add upper half of the next byte to current byte
+        buf[i] += buf[i + 1] >> (8 - n);
+    }
+}
+
+/// Shifts `len` bits of `buf` to the right to
+/// reverse the alignment from `shift_bits`.
+pub fn unshift_bits(buf: &mut [u8], len: usize) {
+    let n = 8 - (len % 8);
+
+    // no shift required when already on a byte boundary
+    if n == 8 {
+        return;
+    }
+
+    let len = buf.len();
+
+    // shift full buffer by n to the right
+    for i in (0..len).rev() {
+        // shift upper half to the right
+        buf[i] >>= n;
+
+        // skip last step for the first byte
+        if i == 0 {
+            continue;
+        }
+
+        // add lower half of previous byte to current byte
+        buf[i] += buf[i - 1] << (8 - n);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn num_bits_test() {
+        let bits = num_bits(0b1100_1111_u8);
+        assert_eq!(bits, 8);
+
+        let bits = num_bits(u16::MAX);
+        assert_eq!(bits, 16);
+
+        let bits = num_bits(0_u64);
+        assert_eq!(bits, 1);
+
+        let bits = num_bits(0b0010_0000_u64);
+        assert_eq!(bits, 6);
+    }
+
+    #[test]
+    fn shift_bits_test() {
+        let mut buf = vec![0b0000_1010];
+        shift_bits(&mut buf, 5);
+        assert_eq!(buf, &[0b0101_0000]);
+
+        let mut buf = vec![0b0000_1010];
+        shift_bits(&mut buf, 8);
+        assert_eq!(buf, &[0b0000_1010]);
+
+        let mut buf = vec![0, 0b0110_1010];
+        shift_bits(&mut buf, 13);
+        assert_eq!(buf, &[0b0000_0011, 0b0101_0000]);
+    }
+
+    #[test]
+    fn unshift_bits_test() {
+        let mut buf = vec![0b0101_0000];
+        unshift_bits(&mut buf, 5);
+        assert_eq!(buf, &[0b0000_1010]);
+
+        let mut buf = vec![0b0000_1010];
+        unshift_bits(&mut buf, 8);
+        assert_eq!(buf, &[0b0000_1010]);
+
+        let mut buf = vec![0b0000_0011, 0b0101_0000];
+        unshift_bits(&mut buf, 13);
+        assert_eq!(buf, &[0, 0b0110_1010]);
+    }
+}
