252fe954911bc2223a7e9444c98b1ca105c77df1
ArckyPN <archsyker@gmail.com>
2025-10-31 11:27:24 +0100

    added docs

--- moqt-rs/src/types/misc/content_exists.rs
+++ moqt-rs/src/types/misc/content_exists.rs
@@ -1,11 +1,17 @@
 use varint::varint_enum;
 
 varint_enum! {
+    /// Describes whether or not an Object has
+    /// been published on a Track.
     #[derive(Debug, PartialEq, Clone, Copy)]
     #[varint(value = x(i))]
     #[varint::draft_ref(v = 14, rename = "section-9.8-3.5.1")]
     pub enum ContentExists {
+        /// An Object has been published on
+        /// this Track.
         Yes = 0x1,
+        /// No Object has been published on
+        /// this Track.
         No = 0x0,
     }
 }
