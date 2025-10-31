da1da4492e398eaa0a2b420e142eb02d82e54580
ArckyPN <archsyker@gmail.com>
2025-10-31 11:27:53 +0100

    prep for future extensions

--- moqt-rs/src/types/message/go_away.rs
+++ moqt-rs/src/types/message/go_away.rs
@@ -1,16 +1,48 @@
+use url::Url;
 use varint::{VarInt, x};
 
-/// TODO docs
-///
-/// some placeholder stuff
+// TODO
+pub enum GoawayError {}
+
+/// A Goaway message signals that a Session is soon
+/// ending. It may contain an migration URL to allow
+/// recipients to initiate a session migration.
 #[derive(Debug, VarInt, PartialEq, Clone)]
 #[varint::draft_ref(v = 14)]
 pub struct Goaway {
-    /// TODO docs
-    // TODO not specified but should probably be valid utf-8
-    // TODO larger than 8192 bytes => protocol violation
+    /// # Migration URL
+    ///
+    /// This URL is used to initiate a session
+    /// migration.
+    ///
+    /// However, it is possible to be empty. In
+    /// this case no migration can be performed.
     #[varint(length = x(i))]
-    uri: x!(..),
+    url: x!(..),
+}
+
+impl Goaway {
+    pub fn new<T>(url: T) -> Self
+    where
+        T: Into<x!(..)>,
+    {
+        Self::from(url)
+    }
+
+    pub fn migration_url(&self) -> Result<Option<Url>, GoawayError> {
+        // TODO not specified but should probably be valid utf-8
+        // TODO larger than 8192 bytes => protocol violation
+        todo!("# TODO validate url and return url is it exists (length could be 0 with no data)")
+    }
+}
+
+impl<T> From<T> for Goaway
+where
+    T: Into<x!(..)>,
+{
+    fn from(value: T) -> Self {
+        Self { url: value.into() }
+    }
 }
 
 #[cfg(test)]
@@ -21,9 +53,7 @@ mod tests {
 
     impl TestData for Goaway {
         fn test_data() -> Vec<(Self, Vec<u8>, usize)> {
-            let v1 = Self {
-                uri: "http:".into(),
-            };
+            let v1 = Self::new("http:");
             let b1 = [
                 vec![5],           // uri length
                 b"http:".to_vec(), // uri
