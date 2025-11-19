2487938be9b26c4b3633f2f19f1e81d341cc89b6
ArckyPN <archsyker@gmail.com>
2025-11-19 11:09:14 +0100

    added custom bon builder

--- moqt-rs/src/types/message/publish/ok.rs
+++ moqt-rs/src/types/message/publish/ok.rs
@@ -12,7 +12,13 @@ use {
 /// Response to a successful [Publish](crate::types::message::Publish).
 #[derive(Debug, VarInt, PartialEq, Clone)]
 #[varint::draft_ref(v = 14)]
-#[varint(parameters(delivery_timeout))]
+#[varint(
+    parameters(delivery_timeout),
+    builder = with_next_group_start,
+    builder = with_largest_object,
+    builder = with_absolute_start,
+    builder = with_absolute_range,
+)]
 pub struct PublishOk {
     /// ## Request ID
     pub request_id: x!(i),
@@ -79,32 +85,222 @@ pub struct PublishOk {
 
 #[bon]
 impl PublishOk {
-    #[builder] // TODO need custom builder
-    pub fn new(
+    #[builder(finish_fn = build)]
+    pub fn with_next_group_start(
         #[builder(field)] parameters: Parameters,
+
+        #[builder(into, setters(
+            name = id,
+            doc {
+                /// Sets the request ID on [PublishOk].
+            }
+        ))]
         request_id: x!(i),
+
+        #[builder(into, setters(
+            doc {
+                /// Sets the forwarding on [PublishOk].
+            }
+        ))]
         forward: Forward,
+
         #[builder(
+            name = sub_prio,
             with = |p: u8| <x!(8)>::try_from(p).expect("u8 will fit into 8 bits"), 
             setters(
                 doc {
-                    /// TODO docs
+                    /// Sets the subscriber priority on [PublishOk].
                 }
         ))]
         subscriber_priority: x!(8),
+
+        #[builder(setters (
+            doc {
+                /// Sets the group order on [PublishOk].
+            }
+        ))]
         group_order: GroupOrder,
-        filter_type: FilterType,
-        start_location: x!([Location]),
-        end_group: x!([i]),
     ) -> Self {
         Self {
             request_id,
             forward,
             subscriber_priority,
             group_order,
-            filter_type,
-            start_location,
-            end_group,
+            filter_type: FilterType::NextGroupStart,
+            start_location: None,
+            end_group: None,
+            parameters,
+        }
+    }
+
+    #[builder(finish_fn = build)]
+    pub fn with_largest_object(
+        #[builder(field)] parameters: Parameters,
+
+        #[builder(into, setters(
+            name = id,
+            doc {
+                /// Sets the request ID on [PublishOk].
+            }
+        ))]
+        request_id: x!(i),
+
+        #[builder(into, setters(
+            doc {
+                /// Sets the forwarding on [PublishOk].
+            }
+        ))]
+        forward: Forward,
+
+        #[builder(
+            name = sub_prio,
+            with = |p: u8| <x!(8)>::try_from(p).expect("u8 will fit into 8 bits"), 
+            setters(
+                doc {
+                    /// Sets the subscriber priority on [PublishOk].
+                }
+        ))]
+        subscriber_priority: x!(8),
+
+        #[builder(setters (
+            doc {
+                /// Sets the group order on [PublishOk].
+            }
+        ))]
+        group_order: GroupOrder,
+    ) -> Self {
+        Self {
+            request_id,
+            forward,
+            subscriber_priority,
+            group_order,
+            filter_type: FilterType::LargestObject,
+            start_location: None,
+            end_group: None,
+            parameters,
+        }
+    }
+
+    #[builder(finish_fn = build)]
+    pub fn with_absolute_start(
+        #[builder(field)] parameters: Parameters,
+
+        #[builder(into, setters(
+            name = id,
+            doc {
+                /// Sets the request ID on [PublishOk].
+            }
+        ))]
+        request_id: x!(i),
+
+        #[builder(into, setters(
+            doc {
+                /// Sets the forwarding on [PublishOk].
+            }
+        ))]
+        forward: Forward,
+
+        #[builder(
+            name = sub_prio,
+            with = |p: u8| <x!(8)>::try_from(p).expect("u8 will fit into 8 bits"), 
+            setters(
+                doc {
+                    /// Sets the subscriber priority on [PublishOk].
+                }
+        ))]
+        subscriber_priority: x!(8),
+
+        #[builder(setters (
+            doc {
+                /// Sets the group order on [PublishOk].
+            }
+        ))]
+        group_order: GroupOrder,
+
+        #[builder(
+            with = |group: impl Into<varint::x!(i)>, object: impl Into<varint::x!(i)>| (group.into(), object.into()).into(),
+            setters(
+            doc {
+                /// Sets the start location on [PublishOk].
+            }
+        ))]
+        start_location: Location,
+    ) -> Self {
+        Self {
+            request_id,
+            forward,
+            subscriber_priority,
+            group_order,
+            filter_type: FilterType::AbsoluteStart,
+            start_location: Some(start_location),
+            end_group: None,
+            parameters,
+        }
+    }
+
+    #[builder(finish_fn = build)]
+    pub fn with_absolute_range(
+        #[builder(field)] parameters: Parameters,
+
+        #[builder(into, setters(
+            name = id,
+            doc {
+                /// Sets the request ID on [PublishOk].
+            }
+        ))]
+        request_id: x!(i),
+
+        #[builder(into, setters(
+            doc {
+                /// Sets the forwarding on [PublishOk].
+            }
+        ))]
+        forward: Forward,
+
+        #[builder(
+            name = sub_prio,
+            with = |p: u8| <x!(8)>::try_from(p).expect("u8 will fit into 8 bits"), 
+            setters(
+                doc {
+                    /// Sets the subscriber priority on [PublishOk].
+                }
+        ))]
+        subscriber_priority: x!(8),
+
+        #[builder(setters (
+            doc {
+                /// Sets the group order on [PublishOk].
+            }
+        ))]
+        group_order: GroupOrder,
+
+        #[builder(
+            name = start,
+            with = |group: impl Into<varint::x!(i)>, object: impl Into<varint::x!(i)>| (group.into(), object.into()).into(),
+            setters(
+            doc {
+                /// Sets the start location on [PublishOk].
+            }
+        ))]
+        start_location: Location,
+
+        #[builder(
+            into,
+            setters(
+            doc {
+                /// Sets the start location on [PublishOk].
+            }
+        ))]
+        end_group: x!(i),
+    ) -> Self {
+        Self {
+            request_id,
+            forward,
+            subscriber_priority,
+            group_order,
+            filter_type: FilterType::AbsoluteRange,
+            start_location: Some(start_location),
+            end_group: Some(end_group),
             parameters,
         }
     }
@@ -118,16 +314,14 @@ mod tests {
 
     impl TestData for PublishOk {
         fn test_data() -> Vec<(Self, Vec<u8>, usize)> {
-            let v1 = Self {
-                request_id: 9u8.into(),
-                forward: Forward::Disabled,
-                subscriber_priority: 35.try_into().expect("will fit"),
-                group_order: GroupOrder::Original,
-                filter_type: FilterType::AbsoluteRange,
-                start_location: Some((3u8, 1u8).into()),
-                end_group: Some(50u8.into()),
-                parameters: Default::default(),
-            };
+            let v1 = Self::with_absolute_range()
+                .id(9u8)
+                .forward(false)
+                .sub_prio(35)
+                .group_order(GroupOrder::Original)
+                .start(3u8, 1u8)
+                .end_group(50u8)
+                .build();
             let b1 = vec![
                 9,  // ID 9
                 0,  // forward disabled
@@ -141,16 +335,13 @@ mod tests {
             ];
             let l1 = b1.len() * 8;
 
-            let v2 = Self {
-                request_id: 10u8.into(),
-                forward: Forward::Enabled,
-                subscriber_priority: 5.try_into().expect("will fit"),
-                group_order: GroupOrder::Ascending,
-                filter_type: FilterType::AbsoluteStart,
-                start_location: Some((1u8, 1u8).into()),
-                end_group: None,
-                parameters: Default::default(),
-            };
+            let v2 = Self::with_absolute_start()
+                .id(10u8)
+                .forward(true)
+                .sub_prio(5)
+                .group_order(GroupOrder::Ascending)
+                .start_location(1u8, 1u8)
+                .build();
             let b2 = vec![
                 10, // ID
                 1,  // forward enabled
@@ -164,16 +355,12 @@ mod tests {
             ];
             let l2 = b2.len() * 8;
 
-            let v3 = Self {
-                request_id: 10u8.into(),
-                forward: Forward::Enabled,
-                subscriber_priority: 5.try_into().expect("will fit"),
-                group_order: GroupOrder::Ascending,
-                filter_type: FilterType::NextGroupStart,
-                start_location: None,
-                end_group: None,
-                parameters: Default::default(),
-            };
+            let v3 = Self::with_next_group_start()
+                .id(10u8)
+                .forward(true)
+                .sub_prio(5)
+                .group_order(GroupOrder::Ascending)
+                .build();
             let b3 = vec![
                 10, // ID
                 1,  // forward enabled
